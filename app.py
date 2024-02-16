44152d05b9d57ed49c560a6e7f5015a46033531c
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2024-02-16 20:25:10 +0100

    installing model

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import streamlit as st
 from streamlit_chat import message
 from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.embeddings import huggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.llms import CTransformers
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 
 #load the pdf
-loader=DirectoryLoader("data\fitness_data.pdf",glob="*.pdf",loader_cls=PyPDFLoader)
+loader=DirectoryLoader("data/",glob="*.pdf",loader_cls=PyPDFLoader)
 documents=loader.load()
 
 #split text into chunks
@@ -17,14 +17,14 @@ text_splitter=RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
 text_chunks=text_splitter.split_documents(documents)
 
 #create embeddings (we will use a sentence transformer from huggingface)
-embeddings=huggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+embeddings=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
                                  model_kwargs={'device':'cpu'})
 
 #create a vector store
 vector_store=FAISS.from_documents(text_chunks,embeddings)
 
 #create llm
-llm=CTransformers(model="EleutherAI/llama-lm",
+llm=CTransformers(model="llama-2-7b-chat.ggmlv3.q4_0.bin",
                   model_type="llama",
                   config={'max_new_tokens':128,"temperature":0.01})
 
